a97a28eee1c644c1a878975844b01c318a29b425
Sahil Mehta <samehta91@gmail.com>
2018-06-07 13:39:03 -0700

    Add viewport tag and remove unnecessary code

--- app/controllers/tv_shows_controller.rb
+++ app/controllers/tv_shows_controller.rb
@@ -4,8 +4,10 @@ class TvShowsController < ApplicationController
   end
 
   def search
+    @results = Tmdb::Search.tv(params[:text])
   end
 
   def show
+    @result = Tmdb::TV.detail(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'search', to: 'tv_shows#search'
-
   get 'tv-show', to: 'tv_shows#show'
 
   root to: 'tv_shows#index'
